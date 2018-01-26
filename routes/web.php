0a74d9186e97ca6beee5660d69b3c03c5506f356
Ralph Cacho <cacho.rc@gmail.com>
2018-01-25 17:54:27 -0800

    Routes: Add route to thread index and show pages

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/threads', 'ThreadsController@index');
+Route::get('/threads/{thread}', 'ThreadsController@show');
