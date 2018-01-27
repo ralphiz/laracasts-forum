0d48fc3b20adb7a8273a759d255ac56fc58a7bbb
Ralph Cacho <cacho.rc@gmail.com>
2018-01-26 21:35:24 -0800

    Threads: Refactor reply to a partial

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -19,19 +19,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             @foreach ($thread->replies as $reply)
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <a href="#">
-                        {{ $reply->owner->name }}
-                    </a> said {{ $reply->created_at->diffForHumans() }}...
-                </div>
-
-                <div class="panel-body">
-                    <article>
-                        {{ $reply->body }}
-                    </article>
-                </div>
-            </div>
+                @include ('threads.reply')
             @endforeach
         </div>
     </div>
