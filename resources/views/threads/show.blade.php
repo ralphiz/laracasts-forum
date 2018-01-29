c965c055c3970c1cd28ec3bdda75f641cd620081
Ralph Cacho <cacho.rc@gmail.com>
2018-01-28 20:30:20 -0800

    Show Thread: Add reply form that shows if the user is logged in, otherwise show links to sign and and register

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -26,5 +26,22 @@
             @endforeach
         </div>
     </div>
+
+    @if(auth()->check())
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <form method="POST" action="{{ $thread->path() . '/replies' }}">
+                    {{ csrf_field() }}
+                    <div class="form-group">
+                        <textarea name="body" id="body" class="form-control" placeholder="Add a reply.." rows="5"></textarea>
+                    </div>
+
+                    <button type="submit" class="btn btn-default">Post</button>
+                </form>
+            </div>
+        </div>
+    @else
+        <p class="text-center">Please <a href="{{ route('login') }}">sign in</a> or <a href="{{ route('register') }}">register</a> to post a reply.</p>
+    @endif
 </div>
 @endsection
