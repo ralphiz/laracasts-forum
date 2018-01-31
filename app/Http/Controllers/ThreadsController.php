4904ca8050e68012d4634e7dcf397127e20c0431
Ralph Cacho <cacho.rc@gmail.com>
2018-01-30 20:21:49 -0800

    Threads Controller: Add auth() middleware to constructor and store() method

--- app/Http/Controllers/ThreadsController.php
+++ app/Http/Controllers/ThreadsController.php
@@ -7,6 +7,19 @@ use Illuminate\Http\Request;
 
 class ThreadsController extends Controller
 {
+    /**
+     * ThreadsController constructor.
+     */
+    public function __construct()
+    {
+        $this->middleware('auth')->only('store');
+    }
+
+    /**
+     * Display a listing of the resource.
+     * 
+     * @return \Illuminate\Http\Response
+     */
     public function index()
     {
         $threads = Thread::latest()->get();
@@ -14,6 +27,29 @@ class ThreadsController extends Controller
         return view('threads.index', compact('threads'));
     }
 
+    /**
+     *  Store a newly created resource in storage.
+     * 
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $thread = Thread::create([
+            'user_id' => auth()->id(),
+            'title' => request('title'),
+            'body' => request('body')
+        ]);
+
+        return redirect($thread->path());
+    }
+
+    /**
+     * Display the specified resource.
+     * 
+     * @param  \App\Thread  $thread
+     * @return \Illuminate\Http\Response
+     */
     public function show(Thread $thread)
     {
         return view('threads.show', compact('thread'));
