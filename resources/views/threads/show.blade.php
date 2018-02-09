aa08dafb59193619613750a7a60956fec3f19ac2
Ralph Cacho <cacho.rc@gmail.com>
2018-02-08 22:35:19 -0800

    Show Thread: Front-end refactoring. [/e16]

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -1,47 +1,54 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <a href="#">{{ $thread->creator->name }}</a> posted:
-                    {{ $thread->title }}
-                </div>
-
-                <div class="panel-body">
-                    <article>
-                        {{ $thread->body }}
-                    </article>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            @foreach ($thread->replies as $reply)
-                @include ('threads.reply')
-            @endforeach
-        </div>
-    </div>
-
-    @if(auth()->check())
+    <div class="container">
         <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <form method="POST" action="{{ $thread->path() . '/replies' }}">
-                    {{ csrf_field() }}
-                    <div class="form-group">
-                        <textarea name="body" id="body" class="form-control" placeholder="Add a reply.." rows="5"></textarea>
+            <div class="col-md-8">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <a href="#">{{ $thread->creator->name }}</a> posted:
+                        {{ $thread->title }}
                     </div>
 
-                    <button type="submit" class="btn btn-default">Post</button>
-                </form>
+                    <div class="panel-body">
+                        {{ $thread->body }}
+                    </div>
+                </div>
+
+                @foreach ($replies as $reply)
+                    @include ('threads.reply')
+                @endforeach
+
+                {{ $replies->links() }}
+
+                @if (auth()->check())
+                    <form method="POST" action="{{ $thread->path() . '/replies' }}">
+                        {{ csrf_field() }}
+
+                        <div class="form-group">
+                            <textarea name="body" id="body" class="form-control" placeholder="Have something to say?"
+                                      rows="5"></textarea>
+                        </div>
+
+                        <button type="submit" class="btn btn-default">Post</button>
+                    </form>
+                @else
+                    <p class="text-center">Please <a href="{{ route('login') }}">sign in</a> to participate in this
+                        discussion.</p>
+                @endif
+            </div>
+
+            <div class="col-md-4">
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        <p>
+                            This thread was published {{ $thread->created_at->diffForHumans() }} by
+                            <a href="#">{{ $thread->creator->name }}</a>, and currently
+                            has {{ $thread->replies_count }} {{ str_plural('comment', $thread->replies_count) }}.
+                        </p>
+                    </div>
+                </div>
             </div>
         </div>
-    @else
-        <p class="text-center">Please <a href="{{ route('login') }}">sign in</a> or <a href="{{ route('register') }}">register</a> to post a reply.</p>
-    @endif
-</div>
+    </div>
 @endsection
