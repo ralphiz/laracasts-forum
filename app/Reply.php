0e21ce0f8a333c8b91b0f064cdc3d1eec1a8664d
Ralph Cacho <cacho.rc@gmail.com>
2018-02-10 20:13:16 -0800

    Reply: Add functionality for favoriting a reply.

--- app/Reply.php
+++ app/Reply.php
@@ -6,10 +6,44 @@ use Illuminate\Database\Eloquent\Model;
 
 class Reply extends Model
 {
+    /**
+     * Don't auto-apply mass assignment protection.
+     *
+     * @var array
+     */
     protected $guarded = [];
 
+    /**
+     * A reply has an owner.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function owner()
     {
         return $this->belongsTo(User::class, 'user_id');
+    }
+
+    /**
+     * A reply can be favorited.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\MorphMany
+     */
+    public function favorites()
+    {
+        return $this->morphMany(Favorite::class, 'favorited');
+    }
+
+    /**
+     * Favorite the current reply.
+     *
+     * @return Model
+     */
+    public function favorite()
+    {
+        $attributes = ['user_id' => auth()->id()];
+
+        if (! $this->favorites()->where($attributes)->exists()) {
+            return $this->favorites()->create($attributes);
+        }
     }
 }
