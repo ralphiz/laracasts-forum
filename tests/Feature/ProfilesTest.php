2d7e9001607b5ac2ff9c715afe1b80d162177b0e
Ralph Cacho <cacho.rc@gmail.com>
2018-03-02 21:03:01 -0800

    ProfileTest: Add test to check if the threads of a user is displayed.

--- tests/Feature/ProfilesTest.php
+++ tests/Feature/ProfilesTest.php
@@ -17,4 +17,17 @@ class ProfileTest extends TestCase
         $this->get("/profiles/{$user->name}")
             ->assertSee($user->name);
     }
+
+    /** @test */
+    function profiles_display_all_threads_created_by_the_associated_user()
+    {
+        $user = create('App\User');
+
+        $thread = create('App\Thread', ['user_id' => $user->id]);
+
+        $this->get("/profiles/{$user->name}")
+            ->assertSee($thread->title)
+            ->assertSee($thread->body);
+
+    }
 }
