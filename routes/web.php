d89fff18650a861076bfcca23d03f5739e654fa3
Ralph Cacho <cacho.rc@gmail.com>
2018-03-02 21:04:00 -0800

    Use a named route for user profile.

--- routes/web.php
+++ routes/web.php
@@ -26,4 +26,4 @@ Route::get('threads/{channel}', 'ThreadsController@index');
 Route::post('/threads/{channel}/{thread}/replies', 'RepliesController@store');
 Route::post('/replies/{reply}/favorites', 'FavoritesController@store');
 
-Route::get('/profiles/{user}', 'ProfilesController@show');
+Route::get('/profiles/{user}', 'ProfilesController@show')->name('profile');
