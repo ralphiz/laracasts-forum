364d5d035a997c3fcf265f98e158d65a0c0b1f7a
Ralph Cacho <cacho.rc@gmail.com>
2018-03-11 00:11:51 -0800

    Add redirect to threads page.

--- app/Http/Controllers/ThreadsController.php
+++ app/Http/Controllers/ThreadsController.php
@@ -91,7 +91,11 @@ class ThreadsController extends Controller
     {
         $thread->delete();
 
-        return response([], 204);
+        if(request()->wantsJson()) {
+            return response([], 204);
+        }
+
+        return redirect('/threads');
     }
 
     /**
