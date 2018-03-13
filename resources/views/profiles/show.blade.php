63d2fd8a62d66ead96b604c713aa5e5cbc6e1099
Ralph Cacho <cacho.rc@gmail.com>
2018-03-12 22:31:04 -0700

    Update views.

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -4,7 +4,6 @@
     <div class="container">
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
-                
                 <div class="page-header">
                     <h1>
                         {{ $profileUser->name }}
@@ -17,8 +16,8 @@
                         <div class="panel-heading">
                             <div class="level">
                                <span class="flex">
-                                    <a href="#">{{ $thread->creator->name }}</a> posted:
-                                   {{ $thread->title }}
+                                    <a href="{{ route('profile', $thread->creator) }}">{{ $thread->creator->name }}</a> posted:
+                                    <a href="{{ $thread->path() }}">{{ $thread->title }}</a>
                                </span>
 
                                 <span>{{ $thread->created_at->diffForHumans() }}</span>
@@ -32,7 +31,6 @@
                 @endforeach
 
                 {{ $threads->links() }}
-  
             </div>
         </div>
     </div>
