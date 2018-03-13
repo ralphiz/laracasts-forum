eff4f0d3885ae01d8fe9aa6c79527d16993913ef
Ralph Cacho <cacho.rc@gmail.com>
2018-03-12 22:30:56 -0700

    Add auth

--- app/Http/Controllers/ThreadsController.php
+++ app/Http/Controllers/ThreadsController.php
@@ -84,11 +84,10 @@ class ThreadsController extends Controller
         ]);
     }
 
-    /**
-     * 
-     */
     public function destroy($channel, Thread $thread)
     {
+        $this->authorize('update', $thread);
+
         $thread->delete();
 
         if(request()->wantsJson()) {
